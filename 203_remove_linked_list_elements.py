276461fbefac75be448fa7d177970ffe0b1e5978
YaohuiZeng <yaohui-zeng@uiowa.edu>
2017-01-08 23:56:08 -0600

    update

--- 203_remove_linked_list_elements.py
+++ 203_remove_linked_list_elements.py
@@ -30,3 +30,14 @@ class Solution(object):
                 prev = curr
             curr = curr.next
         return dummy.next
+
+    def removeElements2(self, head, val):
+        dummy = ListNode(0)
+        dummy.next = head
+        curr = dummy
+        while curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return dummy.next
